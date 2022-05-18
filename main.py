a73a523fdf09f4d32d3cf12f70461709ece13432
Blake Cameron <>
2022-05-18 00:44:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#Author Blake cameron date 17/05/2022 Version 0.1
+def question1 (): 
+ question1 = input(). lower
+
+
+
+print ("kio ora welcome to my quiz whats your name")
+name = input ()
+print ("welcome " + name )
+print ("who is more successful DC or Marvel")
+question1()
+if question1 == "DC":
+  print ("Wrong Dc's rating is around 54% while marvels are mostly around 66%")
+elif question1 == "MARVEL":
+  print ("Correct Marvel's ratings are mostly above 66% and Dc are around 54%")
+
+  question1()
+
